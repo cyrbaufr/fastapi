b9078d537c9040521eca26c6ce19eaf27176337b
cyrbaufr <cyril.baudrillart@gmail.com>
2022-06-10 22:42:19 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -12,6 +12,13 @@ app = FastAPI()
 pickle_in = open("classifier_final.pkl","rb")
 classifier=pickle.load(pickle_in)
 
+def out_of_range(var_name, var_num,
+                msg=" field must take value 0 (No) or 1 (Yes)"):
+    error_msg = var_name + " field must take value 0 (No) or 1 (Yes)"
+    error = {"Error": error_msg}
+    if (var_num>1) or (var_num<0):
+        return error
+
 # 3. Index route, opens automatically on http://127.0.0.1:8000
 @app.get('/')
 def index():
@@ -26,12 +33,18 @@ def predict_creditscore(data:CreditScore):
     EXT_SOURCE_2 = data['EXT_SOURCE_2']
     AMT_CREDIT = data['AMT_CREDIT']
     FLAG_DOCUMENT_3 = data['FLAG_DOCUMENT_3']
+    out_of_range('FLAG_DOCUMENT_3', FLAG_DOCUMENT_3)
     AMT_GOODS_PRICE = data['AMT_GOODS_PRICE']
     CODE_GENDER = data['CODE_GENDER']
+    out_of_range('CODE_GENDER', CODE_GENDER,
+                 msg=" field must take value 0 (Female) or 1 (Male)")
     INSTAL_DAYS_ENTRY_PAYMENT_MAX = data['INSTAL_DAYS_ENTRY_PAYMENT_MAX']
     INSTAL_DAYS_ENTRY_PAYMENT_MEAN = data['INSTAL_DAYS_ENTRY_PAYMENT_MEAN']
     DAYS_EMPLOYED = data['DAYS_EMPLOYED']
     NAME_INCOME_TYPE_Working = data['NAME_INCOME_TYPE_Working']
+    out_of_range('NAME_INCOME_TYPE_Working', NAME_INCOME_TYPE_Working)
+
+
     # print(classifier.predict([[EXT_SOURCE_3, EXT_SOURCE_2,
     #                            AMT_CREDIT, FLAG_DOCUMENT_3,
     #                            AMT_GOODS_PRICE, CODE_GENDER,
