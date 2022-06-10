7eb4ca3cff2ce509b1f5f8ae803038de82bcf852
cyrbaufr <cyril.baudrillart@gmail.com>
2022-06-10 23:41:05 +0200

    Replace Int by StrictInt

--- CreditScores.py
+++ CreditScores.py
@@ -3,17 +3,17 @@ This file indicates the format of features expected
 to predict if a credit wll be reimbursed or not
 """
 
-from pydantic import BaseModel
+from pydantic import (StrictInt, BaseModel)
 
 # Class which describes features used for credit scoring
 class CreditScore(BaseModel):
     EXT_SOURCE_3: float
     EXT_SOURCE_2: float
     AMT_CREDIT: float
-    FLAG_DOCUMENT_3: int
+    FLAG_DOCUMENT_3: StrictInt
     AMT_GOODS_PRICE: float
-    CODE_GENDER: int
+    CODE_GENDER: StrictInt
     INSTAL_DAYS_ENTRY_PAYMENT_MAX: float
     INSTAL_DAYS_ENTRY_PAYMENT_MEAN: float
     DAYS_EMPLOYED: float
-    NAME_INCOME_TYPE_Working: int
+    NAME_INCOME_TYPE_Working: StrictInt
